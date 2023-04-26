01c42b7e36b0572c1a91c48bd52dc955adbd452e
Matt Robb <mr.obb@outlook.com>
2023-04-26 17:46:41 -0400

    Added adapter cloudflare support

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,10 +2,19 @@
 // for information about these interfaces
 declare global {
   namespace App {
-    // interface Error {}
-    // interface Locals {}
-    // interface PageData {}
-    // interface Platform {}
+    interface Locals {}
+
+    interface Platform {
+      env: {}
+      context: {
+        waitUntil(promise: Promise<any>): void;
+      }
+      caches: CacheStorage & { default: Cache }
+    }
+
+    interface Session {}
+
+    interface Stuff {}
   }
 }
 
